53d47f0896b17f0bdd348d8789e10a75ca6843b1
ManEnfu <manenfu@protonmail.com>
2021-08-01 23:22:41 +0700

    awesome: add client spawning rules

--- awesome/rules.lua
+++ awesome/rules.lua
@@ -74,4 +74,21 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+    { 
+        rule_any = { class = { "Firefox", "qutebrowser", "firefox" } }, 
+        properties = { tag = "web" }
+    },
+    { 
+        rule_any = { class = { "pcmanfm-qt" } }, 
+        properties = { tag = "file" }
+    },
+    { 
+        rule_any = { class = { "Virt-manager", "virt-manager" } }, 
+        properties = { tag = "virt" }
+    },
+    { 
+        rule_any = { class = { "mpv", "vlc" } }, 
+        properties = { tag = "media", focus = true },
+        callback = function(c) c:jump_to() end
+    }
 }

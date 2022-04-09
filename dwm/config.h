87b7da1754dee824705ed4662364f97c7d2bdf67
ManEnfu <manenfu@protonmail.com>
2022-04-09 15:51:06 +0700

    dwm: remove dwm-systray patch

--- dwm/config.h
+++ dwm/config.h
@@ -3,15 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 32;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Ubuntu Nerd Font:size=8" };
+static const char *fonts[]          = { "Ubuntu:size=8" };
 static const char dmenufont[]       = "Ubuntu:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";

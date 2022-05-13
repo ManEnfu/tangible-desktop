0780affae72a3a228bb77275c5aee300a88aa7fc
ManEnfu <manenfu@protonmail.com>
2022-05-13 20:41:44 +0700

    awesome: add keybindings for taglist

--- awesome/widgets/taglist.lua
+++ awesome/widgets/taglist.lua
@@ -25,13 +25,18 @@ local taglist_buttons = gears.table.join(
         end
     end),
     awful.button({ "Control" }, 1, awful.tag.viewtoggle),
-    awful.button({ modkey, "Control" }, 1, function(t)
-        if client.focus then
-            client.focus:toggle_tag(t)
-        end
-    end),
+    -- awful.button({ modkey, "Control" }, 1, function(t)
+    --     if client.focus then
+    --         client.focus:toggle_tag(t)
+    --     end
+    -- end),
     awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end),
+    awful.button({ "Shift" }, 1, function(t)
+        if client.focus then
+            client.focus:move_to_tag(t)
+        end
+    end)
 )
 
 -------------------------------------------------------------------------------

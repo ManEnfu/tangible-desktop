4835a10936891e57c3943784b8d87f1fe989ab6f
ManEnfu <manenfu@protonmail.com>
2022-05-13 20:44:56 +0700

    autostart picom with experimental backend

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -6,9 +6,20 @@ function run {
     fi
 }
 
+amixer -D pulse > /dev/null
+
 run ~/.fehbg
-run picom
+run picom --experimental-backend
 run nm-applet
 run lxpolkit
+# run lxqt-policykit-agent
 
 [[ $XDG_SESSION_DESKTOP == "dwm" ]] && run dwmblocks
+if [[ $XDG_SESSION_DESKTOP == "wmaker" ]]; then
+    run stalonetray --dockapp-mode wmaker --slot-size 32 --geometry 2x2 \
+        --parent-bg --scrollbars none
+    run wmblueclock
+    run wmbluemem
+    run wmbluecpu
+    run wmamixer -w
+fi

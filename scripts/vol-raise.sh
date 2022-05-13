167b4ebd4585649be50e929ff0f6d10b7d626e9f
ManEnfu <manenfu@protonmail.com>
2022-05-13 20:45:37 +0700

    update vol-raise

--- scripts/vol-raise.sh
+++ scripts/vol-raise.sh
@@ -2,6 +2,6 @@
 
 amixer -q -D pulse sset Master playback 5%+
 sleep 0.05
-[[ $XDG_SESSION_DESKTOP == "awesome" ]] && awesome-client "myvolume:force_update()" 
+[[ $XDG_SESSION_DESKTOP == "awesome" ]] && awesome-client "awesome.emit_signal('signal::volume')"
 [[ $XDG_SESSION_DESKTOP == "dwm" ]] && pkill -RTMIN+10 dwmblocks
 aplay -q ~/.config/scripts/sound_click_tick.wav

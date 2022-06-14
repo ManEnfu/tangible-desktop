43dffee6fd5ec3f7f21e73232c70ff149bbd1993
ManEnfu <manenfu@protonmail.com>
2022-06-14 17:30:23 +0700

    update nix module

---
+++ modules/kvantum.nix
@@ -0,0 +1,53 @@
+{ pkgs, config, lib, ... }:
+with lib;
+with builtins;
+let
+  cfg = config.qt.kvantum;
+  configDir = ../.;
+in {
+  options.qt.kvantum = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable";
+    };
+    
+    theme = {
+      package = mkOption {
+        type = types.nullOr types.package;
+        default = null;
+        description = "package";
+      };
+      name = mkOption {
+        type = types.str;
+        default = "";
+        description = "package name";
+      };
+    };
+  };
+
+  config = mkIf (cfg.enable) {
+    home.packages = with pkgs; [
+      libsForQt5.qtstyleplugin-kvantum
+      cfg.theme.package
+    ];
+
+    qt = {
+      platformTheme = "gtk";
+      style = {
+        name = "kvantum";
+      };
+    };
+
+    xdg.configFile = {
+      "Kvantum/kvantum.kvconfig".text = ''
+        [General]
+        theme=${cfg.theme.name}
+      '';
+      "Kvantum/${cfg.theme.name}" = {
+        source = "${cfg.theme.package}/share/Kvantum/${cfg.theme.name}";
+        recursive = true;
+      };
+    };
+  };
+}
---
+++ modules/tangible.nix
@@ -0,0 +1,78 @@
+{ pkgs, config, lib, ... }:
+with lib;
+with builtins;
+let
+  cfg = config.desktop.tangible;
+  configDir = ../.;
+in {
+  options.desktop.tangible = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable";
+    };
+  };
+
+  config = mkIf (cfg.enable) {
+    home.packages = with pkgs; [
+      rofi
+      alacritty
+      lxqt.pcmanfm-qt
+      picom
+      feh
+      redshift
+      networkmanagerapplet
+    ];
+
+    gtk = {
+      enable = true;
+      iconTheme = {
+        package = pkgs.tela-icon-theme;
+        name = "Tela-dark";
+      };
+      theme = {
+        package = pkgs.materia-theme;
+        name = "Materia-dark-compact";
+      };
+      font = {
+        package = pkgs.ubuntu_font_family;
+        name = "Ubuntu";
+        size = 8;
+      };
+    };
+
+    qt = {
+      enable = true;
+      kvantum = {
+        enable = true;
+        theme = {
+          package = pkgs.materia-kde-theme;
+          name = "MateriaDark";
+        };
+      };
+    };
+
+    xdg.configFile = {
+      "awesome" = {
+        source = "${configDir}/awesome";
+        recursive = true;
+      };
+      "alacritty" = {
+        source = "${configDir}/alacritty";
+        recursive = true;
+      };
+      "picom" = {
+        source = "${configDir}/picom";
+        recursive = true;
+      };
+      "rofi" = {
+        source = "${configDir}/rofi";
+        recursive = true;
+      };
+      "scripts" = {
+        source = "${configDir}/scripts";
+        recursive = true;
+      };
+    };
+  };
+}

dc39667061c7d6c5b919ecb0f844947b18d40503
ManEnfu <manenfu@protonmail.com>
2022-06-19 23:06:33 +0700

    fix volume raise

--- scripts/vol-lower.sh
+++ scripts/vol-lower.sh
@@ -4,4 +4,4 @@ pactl set-sink-volume $(pactl get-default-sink) -5%
 sleep 0.05
 [[ $XDG_SESSION_DESKTOP == "none+awesome" ]] && awesome-client "awesome.emit_signal('signal::volume')"
 [[ $XDG_SESSION_DESKTOP == "dwm" ]] && pkill -RTMIN+10 dwmblocks
-paplay -q ~/.config/scripts/sound_click_tick.wav
+paplay ~/.config/scripts/sound_click_tick.wav
--- scripts/vol-raise.sh
+++ scripts/vol-raise.sh
@@ -3,9 +3,9 @@
 pactl set-sink-volume $(pactl get-default-sink) +5%
 # limit to 100%
 pactl get-sink-volume $(pactl get-default-sink) | grep -q -e '1..\%' && \
-    pactl set-sink-volume $(pactl-get-default-sink) 100%
+    pactl set-sink-volume $(pactl get-default-sink) 100%
 
 sleep 0.05
 [[ $XDG_SESSION_DESKTOP == "none+awesome" ]] && awesome-client "awesome.emit_signal('signal::volume')"
 [[ $XDG_SESSION_DESKTOP == "dwm" ]] && pkill -RTMIN+10 dwmblocks
-paplay -q ~/.config/scripts/sound_click_tick.wav
+paplay ~/.config/scripts/sound_click_tick.wav

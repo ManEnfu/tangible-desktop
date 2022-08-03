052c0291d6e6ba4c3dbbba11942d1616cc8d5675
ManEnfu <manenfu@protonmail.com>
2022-08-03 21:22:39 +0700

    waybar: update bar

--- waybar/scripts/hyprland-apptitle.sh
+++ waybar/scripts/hyprland-apptitle.sh
@@ -4,13 +4,18 @@ set -e
 
 handle () {
     if [ ${1:0:12} = "activewindow" ]; then
-        data=${1:14}
-        name=$(echo $data | sed 's/,.*//')
-        echo $name
+        update 
     fi
 }
 
-if [ $XDG_CURRENT_DESKTOP = "hyprland" ]; then
+update() {
+    NAME=$((hyprctl activewindow | grep "title: " | sed "s/.*title: //") || echo "")
+    echo $NAME
+}
+
+update
+
+if pgrep Hyprland > /dev/null; then
     socat - UNIX-CONNECT:/tmp/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock \
         | while read line; do handle $line; done
 fi
